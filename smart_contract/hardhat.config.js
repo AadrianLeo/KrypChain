3e489c1d1899fb0a88ff3df1870e57bd6e118c36
DHARAMPAL SINGH <ravisingh02082001@gmail.com>
2022-05-22 20:30:07 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/yczHc6bGGIb-DqBjgHdcAn2LgRS-fZ_a',
-      accounts: ['f7a6d1ea1004da61fc54a0a52c867722bd3e220962c45916852a0fad2010ce2e'],
+      url: 'ALCHEMPY_APPLICATION_API_KEY',
+      accounts: ['META_MASK_API_PRIVATE_KEY'],
       
     },
   },
