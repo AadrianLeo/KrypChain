9522a194d78a907c518cfd91551c5cb87cade487
DHARAMPAL SINGH <ravisingh02082001@gmail.com>
2022-05-22 20:27:43 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xBDCce39fB1BdECAa61617e9772EF564aBa416B5F";
+export const contractAddress = "transaction_ADDRESS_KEY";
 export const contractABI = abi.abi;
